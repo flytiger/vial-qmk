ed0247ebcd5969f4754edfdd988205b838b596d4
craingermax <crainger@outlook.com>
2021-07-25 14:05:42 +1000

    crainger keymap

---
+++ keyboards/gmmk/pro/keymaps/crainger/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define TAPPING_TOGGLE 2
+// TT set to two taps
+
+#define TAPPING_TERM 165
+
+#define VIA_CUSTOM_LIGHTING_ENABLE
+#define VIA_QMK_RGBLIGHT_ENABLE
+/* Change USB Polling Rate to 1000hz and a larger keys per scan for elite gaming*/
+#define USB_POLLING_INTERVAL_MS 1
